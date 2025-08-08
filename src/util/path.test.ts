d1eb47b9438afda16154be4a2512438d384f85f5
Ben Foxall <benfoxall@gmail.com>
2025-08-08 13:53:30 +0100

    :robot: migrate to vitest

--- src/util/path.test.ts
+++ src/util/path.test.ts
@@ -1,42 +1,41 @@
-import { suite, test } from "mocha";
-import { assert } from "chai";
+import { describe, test, expect } from "vitest";
 
 import { pathsToHeirachy } from "./path";
 
-suite("pathsToHeirachy", () => {
+describe("pathsToHeirachy", () => {
   test("empty list", () => {
     const result = pathsToHeirachy([]);
 
-    assert.deepEqual(result, []);
+    expect(result).toEqual([]);
   });
   test("single file", () => {
     const result = pathsToHeirachy(["file.txt"]);
 
-    assert.deepEqual(result, ["file.txt"]);
+    expect(result).toEqual(["file.txt"]);
   });
 
   test("multi files", () => {
     const result = pathsToHeirachy(["a.txt", "b.txt"]);
 
-    assert.deepEqual(result, ["a.txt", "b.txt"]);
+    expect(result).toEqual(["a.txt", "b.txt"]);
   });
 
   test("hierarchy one dir", () => {
     const result = pathsToHeirachy(["dir/a.txt", "dir/b.txt"]);
 
-    assert.deepEqual(result, [["dir", ["a.txt", "b.txt"]]]);
+    expect(result).toEqual([["dir", ["a.txt", "b.txt"]]]);
   });
 
   test("hierarchy multi", () => {
     const result = pathsToHeirachy(["dir/a.txt", "dir/b.txt", "top.txt"]);
 
-    assert.deepEqual(result, [["dir", ["a.txt", "b.txt"]], "top.txt"]);
+    expect(result).toEqual([["dir", ["a.txt", "b.txt"]], "top.txt"]);
   });
 
   test("hierarchy sorted", () => {
     const result = pathsToHeirachy(["aa/a", "aa/b", "a", "bb/a", "bb/b", "b"]);
 
-    assert.deepEqual(result, [
+    expect(result).toEqual([
       ["aa", ["a", "b"]],
       ["bb", ["a", "b"]],
       "a",
